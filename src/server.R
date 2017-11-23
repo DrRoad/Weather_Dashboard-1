01d409cc00e027fc1ebd91bf29f6ce9229648a43
Mathias <mathias711@gmail.com>
2017-11-23 11:21:47 +0100

    Basic dashboard

---
+++ src/server.R
@@ -0,0 +1,9 @@
+
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(shiny,
+               leaflet)
+
+
+server <- function(input, output, session) {
+
+}
---
+++ src/ui.R
@@ -0,0 +1,45 @@
+if (!require("pacman")) install.packages("pacman")
+library(pacman)
+pacman::p_load(shiny,
+               leaflet,
+               shinydashboard)
+
+
+
+ui <- dashboardPage(title="Weather Dashboard",
+                    dashboardHeader(title= "Weather Dashboard 2.0"),
+                    dashboardSidebar(
+                        conditionalPanel(condition="input.conditionedPanels==1",
+
+                                         # box(title="time relative to now",
+                                         helpText("Unos")
+
+                        ),
+                        conditionalPanel(condition="input.conditionedPanels==2",
+                                         helpText("Dos")
+
+                        ),
+                        conditionalPanel(condition="input.conditionedPanels==3",
+                                         helpText("Tres!")
+                        )
+
+                    ),
+                    dashboardBody({
+
+                        tabsetPanel(
+                            id = "conditionedPanels",
+                            tabPanel('One',
+                                     value=1,
+                                     helpText("Test")
+                            ),
+                            tabPanel('Two',
+                                     value=2,
+                                     helpText("Test2")
+                            ),
+                            tabPanel('Three',
+                                     value=3,
+                                     helpText("Test3")
+                            )
+                        )
+                    })
+)
